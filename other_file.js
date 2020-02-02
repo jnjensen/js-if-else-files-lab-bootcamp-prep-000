6ccdbdf3cc5133dad1b53337c7d5cad6ad836875
James Jensen <jnjensen@live.com>
2020-02-02 05:13:32 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,3 +1,15 @@
 var index = require("./index.js")
 
 console.log(index.name)
+
+if (name === "Susan"){
+  console.log("The name is correct")
+} else {
+  console.log("Expected: Susan, Recieved: " + name)
+}
+
+if (height === "70"){
+  console.log("The height is correct")
+} else {
+  console.log("Expected: 70, Recieved: " + height)
+}
